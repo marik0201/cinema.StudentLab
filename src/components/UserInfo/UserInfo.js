c48470e6b6e08d54e50a4ec70c1af0bd2036131d
Mark Likhtar <Mark.Likhtar@itechart-group.com>
2019-04-18 14:42:50 +0300

    userpage

---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import history from '../../history';
+import style from './style.scss';
+
+export default class UserInfo extends Component {
+  render() {
+    return (
+      <div className=''>
+        info
+      </div>
+      )
+  }
+}
